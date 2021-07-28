571fab401ba0bcf48597aa19cc3045727eb1d485
Kristina Kachailo <krisrom7@gmail.com>
2021-07-28 12:48:28 +0300

    getchar_putchar

---
+++ lesson_6/lect6.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() 
+{
+    char a;
+
+    while ((a = getchar()) != '\n')
+    {
+        if (a >= 'a' && a <= 'z')
+            putchar ('l');
+        else if (a >= 'A' && a <= 'Z')
+            putchar ('u');
+        else if (a >= '0' && a <= '9')
+            putchar ('d');
+        else if (a == 32)
+            putchar (0);
+        else
+            putchar('*');
+    }
+    
+
+    return 0;
+}
